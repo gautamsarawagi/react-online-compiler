a79de1cc95ab8c9771e996efd542e15ba18051ca
Gautam Sarawagi <gautamsarawagi1@gmail.com>
2025-08-25 23:33:05 +0530

    fix: few more fixes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,11 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '', '');
-  
-  const API_URL = env.VITE_API_URL || 'http://localhost:3000';
+export default defineConfig(() => {   
+  const API_URL = 'https://react-online-compiler.onrender.com/';
 
-  return {
+  return {  
     plugins: [react(), tsconfigPaths()],
     server: {
       proxy: {
