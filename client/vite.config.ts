f0a81170d2dcb5603ce5913451cd889ac5a45175
Gautam Sarawagi <gautamsarawagi1@gmail.com>
2025-08-26 06:57:38 +0530

    fix: few more fixes regarding vite delpoyment on netlify

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import netlify from "@netlify/vite-plugin";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), netlify()],
 })
